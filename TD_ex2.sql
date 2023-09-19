aab2442f222ce5c608aacb14e633d04482421ba9
manuvai <rm90266@gmail.com>
2023-09-19 11:53:32 +0200

    feat: Ajout de lignes

--- TD_ex2.sql
+++ TD_ex2.sql
@@ -6,6 +6,19 @@
 -- Groupe (CodeGR, NomGR, PaysOrigineGR)
 -- Reserver (CodeC#, CodeH#, CodeTyCH#, DateArr, DateDep, NombreCH)
 
+-- 1. Pour chaque client, donner sa civilité en clair (Monsieur ou Madame), son nom, son prénom et éventuellement le nom et le domaine de sa société
+-- 2. Nom des sociétés ayant effectué au moins une réservation débutant cette année
+-- 3. Nom des sociétés ayant effectué dix réservations débutant cette année
+-- 4. Nom des sociétés n’ayant effectué aucune réservation en 2018 (arrivées et départs en 2018)
+-- 5. Nom des sociétés ayant effectué toutes réservations débutant cette année
+-- 6. Nom des sociétés n’ayant effectué que des réservations cette année (arrivée et départ cette année)
+-- 7. Nom et prénom des clients ayant effectué une réservation de 2 nuits
+-- 8. Nom et prénom des clients ayant effectué deux réservations en 2019 (ne pas se limiter à une arrivée et un départ cette année)
+-- 9. Nom, prénom et ville du client ayant effectué le plus de réservations débutant en 2018
+-- 10. Nom des groupes hôteliers proposant plus de 10 hôtels dans le pays originaire du groupe
+-- 11. Nom des hôtels français de 3 étoiles proposant tous les types de chambres
+-- 12. Nom des hôtels français de 3 étoiles ne proposant pas le type de chambres « suite »
+
 -- 13. Nom des clients ayant réservé un hôtel dans tous les groupes hôteliers proposés par la BD
 SELECT NomC
 FROM Client Cl
@@ -14,10 +27,12 @@ WHERE NOT EXISTS (
     FROM Groupe Gr
     WHERE 1 = 1
         AND NOT EXISTS (
-        SELECT *
-        FROM Reserver R, Hotel H
-        WHERE R.CodeH = H.CodeH
-            AND H.CodeGR = Gr.CodeGR
-            AND R.CodeC = Cl.CodeC
+            SELECT *
+            FROM Reserver R, Hotel H
+            WHERE R.CodeH = H.CodeH
+                AND H.CodeGR = Gr.CodeGR
+                AND R.CodeC = Cl.CodeC
     )
 )
+
+-- 19. Nom des sociétés ayant réservé tous les types de chambres
