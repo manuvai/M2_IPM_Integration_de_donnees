93e58acc9f6e0581a0753e1f6ec2194f45516d24
Manuvai <44281637+manuvai@users.noreply.github.com>
2023-12-20 01:28:54 +0100

    feat: Ajout du fichier csv output

--- PdfReader/main.py
+++ PdfReader/main.py
@@ -70,8 +70,32 @@ class Page:
     def get_all(self) -> dict:
         return self.data
   
+class CsvConverter:
+    def to_csv(self, lines: list[dict[str, str]], output: str, glue: str = ";") -> None:
+        def __get_headers(data: dict) -> list:
+            if (data is None):
+                return []
+            return [key for key in data.keys()]
+
+        def __get_content(lines: list[dict[str, str]], glue: str):
+            response_lines = []
+            headers = __get_headers(lines[0])
+            response_lines.append(glue.join(headers))
+
+            for line in lines:
+                response_line = glue.join([line.get(key, "") for key in headers])
+                response_lines.append(response_line)
+
+            return "\n".join(response_lines)
+            
+        content = __get_content(lines, glue)
+        
+        with open(output, mode="w+") as f:
+            f.write(content)
+
 # creating a pdf reader object 
 reader = PdfReader('VesselRegisterReport.pdf') 
+pages = []
 
 for i in range(1, len(reader.pages)-1):
      
@@ -80,4 +104,7 @@ for i in range(1, len(reader.pages)-1):
     text = page.extract_text() 
     page = Page(text)
 
-    print(page.get_all()) 
+    pages.append(page.get_all())
+
+converter = CsvConverter()
+converter.to_csv(pages, "output.csv") 
